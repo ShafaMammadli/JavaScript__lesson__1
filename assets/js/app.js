cfffe843f56965eff7a62c73fa8b761d5ac635ac
Şəfa Məmmədli <mmmdlisfa12@gmail.com>
2023-12-18 16:52:29 +0400

    JavaScript__lesson__1

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const userInput = prompt('Who is there?');
+
+if (userInput == 'Cancel') {
+    alert("Canceled");
+}
+else if (userInput == 'Other') {
+    alert('I don`t know you');
+}
+else if (userInput == 'Admin') {
+    const passwordInput = prompt('Password');
+
+    if (passwordInput == 'Cancel') {
+        alert('Canceled');
+    }
+    else if (passwordInput == 'Other') {
+        alert('Wrong password');
+    }
+    else if (passwordInput == 'TheMaster') {
+        alert('Welcome!')
+    }
+    else {
+        alert('Wrong password');
+    }
+}
+else {
+    alert('I don`t know you');
+}
